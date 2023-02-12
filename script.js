ca4052291dcada8c467bfebf9e5eb4586c931dbb
Dev-Arhaan <113898488+Dev-Arhaan@users.noreply.github.com>
2023-02-12 18:37:56 +0000

    Refactored code + separated files

---
+++ script.js
@@ -0,0 +1,25 @@
+ function printSymbol(e){
+        let result = document.getElementById("result");
+        let obj = e.target;
+
+        if (result.value == 0)
+            result.value = obj.textContent;
+        else
+        result.value = result.value + obj.textContent;
+    }
+
+    
+
+    let result = document.getElementById("result");
+    let clear = document.getElementById("clear");
+    let equal = document.getElementById("equal");
+
+    let symbols = document.getElementsByClassName("symbol");
+
+    for (let i = 0; i < symbols.length; i++)
+    symbols[i].addEventListener("click", printSymbol);
+
+    clear.addEventListener("click", 
+    () => document.getElementById("result").value = 0);
+
+    equal.addEventListener("click", e => result.value = eval(result.value));
